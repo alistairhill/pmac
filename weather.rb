2e915be105ca735cd5315b5ab691bff67fee7afd
Alistair <plantonly@gmail.com>
2015-01-03 20:45:12 -0800

    replaced whitespaces > 7 with new value in each data row

--- weather.rb
+++ weather.rb
@@ -3,28 +3,29 @@
 # 2. parse over file
 # 3. put each line into an array
 # 4. delete unneeded rows
-# 5. make rows uniform
+# 5. make rows uniform (rows should have 17 values)
 # 6. assign rows to columns array
 # 7. compare column 2 to column 3 for smallest difference
 # 8. return column number associated with above condition
-
-# Dy MxT   MnT
-# 1  88    59
-# 2  79    63
 
 @array = []
 @row = []
 @column = []
 
-def open_file(file)
-  File.open(file).each do |line|
-    @array << line.split(" ")
+def open_file(file_name)
+  File.open(file_name).each do |line|
+    @array << line.split("\t")
+  end
+end
+
+def add_values_for_tabs
+  @array.each do |row|
+    row.each do |string|
+      string.gsub!(/\s{7,}/, " x ")
+    end
   end
   print @array
 end
 
-
-
-
-
 open_file("data/w_data.dat")
+add_values_for_tabs
