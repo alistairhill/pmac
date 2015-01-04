734aef541d6b44e2caa6b2600b207733ce6e0311
Alistair <plantonly@gmail.com>
2015-01-03 23:42:43 -0800

    cleansed more data and converted rows to columns

--- weather.rb
+++ weather.rb
@@ -9,39 +9,37 @@
 # 8. return column number associated with above condition
 
 @array = []
-@row = []
-@column = []
+@compare = []
 
-def open_file(file_name)
-  File.open(file_name).each do |line|
+def open_file(file)
+  File.open(file).each do |line|
     @array << line.split("\t")
   end
 end
 
-def add_values_for_tabs
+def make_rows_uniform
   @array.map! do |row|
     row.map do |string|
-      string.gsub!(/\s{7,}/, " x ")
-      # string.gsub!(/[A-Z]\s[x]/, " ")
+      string.gsub!(/\s{8,}/, " #{0} ")
+      string.gsub!(/1HrP/, "") #delete header w/ no data
+      string.gsub!(/\*/, "") #delete asterisk
     end
     row.join(" ").split(" ")
   end
 end
 
-def make_rows_uniform
-  @array.each do |row|
-    p row.length
+def delete_unneeded_rows
+  @array.delete_if do |row|
+    row[0] == "<pre>" || row[0] == "</pre>" || row[0] == nil || row[0] == "MMU" || row[0] == "mo"
   end
 end
 
-def delete_unneeded_rows
-  @array.delete_if do |row|
-    row[0] == "<pre>" || row[0] == "</pre>" || row[0] == nil || row[0] == "MMU"
-  end
-  print @array
+def make_columns
+  @columns = @array.transpose
+  p @columns
 end
 
 open_file("data/w_data.dat")
-add_values_for_tabs
-delete_unneeded_rows
 make_rows_uniform
+delete_unneeded_rows
+make_columns
