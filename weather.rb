5d7c1ed5af42eaeef514a6715a2c0982ee25f5e0
Alistair <plantonly@gmail.com>
2015-01-04 18:40:34 -0800

    rename and refactor

--- weather.rb
+++ weather.rb
@@ -11,18 +11,18 @@
 class WeatherData
   def initialize
     @weather_data = []
-    @compare = []
-    @hash = Hash.new(0)
+    @sum_array = []
+    @col_com_hash = Hash.new(0)
   end
 
-  def open_file(file)
-    File.open(file).each do |line|
+  def open_file(file_name)
+    File.open(file_name).each do |line|
       @weather_data << line.split("\t")
     end
-    return make_rows_uniform
+    return cleanse_data
   end
 
-  def make_rows_uniform
+  def cleanse_data
     @weather_data.map! do |row|
       row.map do |string|
         string.gsub!(/\s{8,}/, " #{0} ") #replace >=8 whitespaces w/ 0
@@ -36,31 +36,36 @@ class WeatherData
 
   def delete_unneeded_rows
     @weather_data.delete_if do |row|
-      row[0] == "<pre>" || row[0] == "</pre>" || row[0] == nil || row[0] == "MMU" || row[0] == "mo"
+      row.first == "<pre>" || row.first == "</pre>" || row.first == nil || row.first == "MMU" || row.first == "mo"
     end
     return make_columns
   end
 
   def make_columns
-    @columns = @weather_data.transpose
+    @col_data = @weather_data.transpose
   end
 
-  def min_temp_spread(col_a, col_b, operator)
-    @columns[col_a-1].zip(@columns[col_b-1]).each do |mxt, mnt|
-      @compare << (mxt.to_i.send(operator, mnt.to_i))
+  def col_calculation(col_a, col_b, operator)
+    @col_data[col_a-1].zip(@col_data[col_b-1]).each do |col_1, col_2|
+      @sum_array << (col_1.to_i.send(operator, col_2.to_i))
     end
   end
 
-  def col_comparison
-    #using days column to zip the sum of mxt minus mnt
-    @columns[0].zip(@compare).each do |day, sum|
-      @hash[day] = sum if day != "Dy" #don't include header
+  def choose_result_col(col_name_string)
+    @col_data.each_with_index do |col, index|
+      return col_comparison(index) if col.first == col_name_string
     end
-    return @hash.sort_by {|key, value| value}.first[0]
+  end
+
+  def col_comparison(res_col_num)
+    @col_data[res_col_num].zip(@sum_array).each do |result_col, sum|
+      @col_com_hash[result_col] = sum if result_col != @col_data[res_col_num].first
+    end
+    p @col_com_hash.min_by {|key, value| value}.first
   end
 end
 
 temperature_spread = WeatherData.new
 temperature_spread.open_file("data/w_data.dat")
-temperature_spread.min_temp_spread(2,3, "-")
-p temperature_spread.col_comparison
+temperature_spread.col_calculation(2,3, "-")
+temperature_spread.choose_result_col("Dy")
